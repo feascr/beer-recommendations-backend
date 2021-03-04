a4bda264291a18a7e6fea7c2e4c69e3ffffb9e50
Artem Petrov <apetr654@gmail.com>
2021-03-04 22:23:35 +0300

    Add sign in tests, fix indentation error in serializers

--- beer_recommendations/beer_app/serializers.py
+++ beer_recommendations/beer_app/serializers.py
@@ -143,27 +143,27 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'password']
 
-  def create(self, validated_data):
-    user = User(email=validated_data['email'], username=validated_data['email'])
-    user.set_password(validated_data['password'])
-    # queryset = Beer.objects.all().annotate(count=Count('beerreview__review_overall')).filter(count__gt=1000).annotate(average_rate=Avg('beerreview__review_overall')).order_by(F('average_rate').desc(nulls_last=True))[:10]
-    queryset = BeerReview.objects.all().values('review_beer').annotate(count=Count('review_overall')).filter(count__gt=1000).values('review_beer').annotate(average_rate=Avg('review_overall')).order_by(F('average_rate').desc(nulls_last=True)).values_list('review_beer')[:10]
-    # queryset = Beer.objects.all()
-    # count = queryset.aggregate(count=Count('id'))['count']
-    # top_beers = []
-    # for i in range(10):
-    #   random_index = randint(0, count - 1)
-    #   top_beers.append(queryset[random_index])
-    recommendations = BeerRecommendation(recommendation_user=user, top1_beer=Beer.objects.get(id=queryset[0][0]),
-                                                                   top2_beer=Beer.objects.get(id=queryset[1][0]),
-                                                                   top3_beer=Beer.objects.get(id=queryset[2][0]),
-                                                                   top4_beer=Beer.objects.get(id=queryset[3][0]),
-                                                                   top5_beer=Beer.objects.get(id=queryset[4][0]),
-                                                                   top6_beer=Beer.objects.get(id=queryset[5][0]),
-                                                                   top7_beer=Beer.objects.get(id=queryset[6][0]),
-                                                                   top8_beer=Beer.objects.get(id=queryset[7][0]),
-                                                                   top9_beer=Beer.objects.get(id=queryset[8][0]),
-                                                                   top10_beer=Beer.objects.get(id=[9][0]))
-    user.save()
-    recommendations.save()
-    return user
+    def create(self, validated_data):
+        user = User(email=validated_data['email'], username=validated_data['email'])
+        user.set_password(validated_data['password'])
+        # queryset = Beer.objects.all().annotate(count=Count('beerreview__review_overall')).filter(count__gt=1000).annotate(average_rate=Avg('beerreview__review_overall')).order_by(F('average_rate').desc(nulls_last=True))[:10]
+        queryset = BeerReview.objects.all().values('review_beer').annotate(count=Count('review_overall')).filter(count__gt=1000).values('review_beer').annotate(average_rate=Avg('review_overall')).order_by(F('average_rate').desc(nulls_last=True)).values_list('review_beer')[:10]
+        # queryset = Beer.objects.all()
+        # count = queryset.aggregate(count=Count('id'))['count']
+        # top_beers = []
+        # for i in range(10):
+        #   random_index = randint(0, count - 1)
+        #   top_beers.append(queryset[random_index])
+        recommendations = BeerRecommendation(recommendation_user=user, top1_beer=Beer.objects.get(id=queryset[0][0]),
+                                                                       top2_beer=Beer.objects.get(id=queryset[1][0]),
+                                                                       top3_beer=Beer.objects.get(id=queryset[2][0]),
+                                                                       top4_beer=Beer.objects.get(id=queryset[3][0]),
+                                                                       top5_beer=Beer.objects.get(id=queryset[4][0]),
+                                                                       top6_beer=Beer.objects.get(id=queryset[5][0]),
+                                                                       top7_beer=Beer.objects.get(id=queryset[6][0]),
+                                                                       top8_beer=Beer.objects.get(id=queryset[7][0]),
+                                                                       top9_beer=Beer.objects.get(id=queryset[8][0]),
+                                                                       top10_beer=Beer.objects.get(id=[9][0]))
+        user.save()
+        recommendations.save()
+        return user
--- beer_recommendations/beer_app/test/test_auth.py
+++ beer_recommendations/beer_app/test/test_auth.py
@@ -60,6 +60,22 @@ class SignUpTests(APITestCase):
                                         })
         self.assertEqual(User.objects.count(), user_counts)
 
+    def test_create_account_without_field(self):
+        """
+        Test attempt to register User without required fields in request body.
+        """
+        user_counts = User.objects.count()
+        url = '/registration'
+        data = {'email_fake': 'test_str'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # TODO: try to bypass implicit ErrorDetail instansiation
+        self.assertEqual(response.data, {
+                                            'email': [ErrorDetail('This field is required.', code='required')], 
+                                            'password': [ErrorDetail('This field is required.', code='required')]
+                                        })
+        self.assertEqual(User.objects.count(), user_counts)
+
     def test_create_account_with_same_email(self):
         """
         Test attempt to register User with already exist email.
@@ -78,3 +94,42 @@ class SignUpTests(APITestCase):
         self.assertEqual(response.data, {'email': [ErrorDetail('This field must be unique.', code='unique')]})
         self.assertEqual(User.objects.count(), user_counts)
         self.assertEqual(User.objects.latest('id').id, user_id)
+
+    def test_sign_in_with_valid_data(self):
+        """
+        Test attempt to sing in User with valid data.
+        """
+        url = '/registration'
+        data = {'email': 'test@user.com', 'password': 'test_password'}
+        response = self.client.post(url, data, format='json')
+
+        url = '/api-token-auth'
+        data = {'username': 'test@user.com', 'password': 'test_password'}
+        response = self.client.post(url, data, format='json')
+
+        user_token = str(User.objects.latest('id').auth_token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # TODO: try to bypass implicit ErrorDetail instansiation
+        self.assertEqual(response.data, {'token': user_token})
+
+    def test_sign_in_with_invalid_password(self):
+        """
+        Test attempt to sing in User with invalid password.
+        """
+        url = '/registration'
+        data = {'email': 'test@user.com', 'password': 'test_password2'}
+        response = self.client.post(url, data, format='json')
+
+        url = '/api-token-auth'
+        data = {'username': 'test@user.com', 'password': 'test_password'}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # TODO: try to bypass implicit ErrorDetail instansiation
+        self.assertEqual(response.data, {'non_field_errors': [ErrorDetail('Unable to log in with provided credentials.', code='authorization')]})
+
+    """
+    TODO:
+        - Test sign in with blank fields
+        - Test sign in without fields
+    """
